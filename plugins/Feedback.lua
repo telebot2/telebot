7275302ff4f02dbe18ba79ca6681c4670adaf81f
AliKhodadad <alikhodadad44@gmail.com>
2016-02-05 18:48:21 +0330

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -21,7 +21,7 @@ local fuses = '!printf user#id' .. msg.from.id
 
                  local sends0 = send_msg('chat#اینجا ای دی گروه فید بک', fuse, ok_cb, false)
 
- return 'your feedback succesfully recived to @creed_is_dead and Team😜!'
+ return 'your feedback succesfully recived to @DARK_BOYS_ADMIN😜!'
 
      
 
