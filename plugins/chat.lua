165eb65d6021bb435f36f236c200eae852b5f6d2
AliKhodadad <alikhodadad44@gmail.com>
2016-02-05 20:55:27 +0330

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,22 +1,4 @@
 local function run(msg)
-if msg.text == "علی دارک" then
-	return "بابای منو چیکار داری"
-end
-if msg.text == "اشرار" then
-	return "نگاییدم"
-end
-if msg.text == "عقری شر" then
-	return "کس ننش"
-end
-if msg.text == "Ali" then
-	return "ba babaye man chikar dari"
-end
-if msg.text == "ali" then
-	return "ba babaye man chikar dari"
-end
-if msg.text == "salam telewild" then
-	return "salam golam"
-end
 if msg.text == "hi" then
 	return "Hello bb"
 end
@@ -30,25 +12,16 @@ if msg.text == "hello" then
 	return "Hi honey"
 end
 if msg.text == "Salam" then
-	return "Salam"
+	return "Salam aleykom"
 end
 if msg.text == "salam" then
 	return "salam"
-end
-if msg.text == "dark" then
-	return "همیشه بهترینه"
-end
-if msg.text == "Dark" then
-	return "همیشه بهترینه"
-end
-if msg.text == "DARK" then
-	return "همیشه بهترینه"
 end
 if msg.text == "Telewild" then
 	return "Yes?"
 end
 if msg.text == "telewild" then
-	return "guuuun"
+	return "What?"
 end
 if msg.text == "bot" then
 	return "hum?"
@@ -56,8 +29,8 @@ end
 if msg.text == "Bot" then
 	return "Hum?"
 end
-if msg.text == "ربات" then
-	return "guuun"
+if msg.text == "?" then
+	return "Hum??"
 end
 if msg.text == "Bye" then
 	return "Babay"
@@ -73,10 +46,8 @@ return {
 	patterns = {
 		"^[Hh]i$",
 		"^[Hh]ello$",
-		"^[Zz]ac$",
-		"^ZAC$",
 		"^[Bb]ot$",
-		"^[Uu]mbrella$",
+		"^[Tt]elewild$",
 		"^[Bb]ye$",
 		"^?$",
 		"^[Ss]alam$",
