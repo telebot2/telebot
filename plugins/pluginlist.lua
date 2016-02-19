cc13806bc5d4ab81334494e34f3565f9f701493f
AliKhodadad <alikhodadad44@gmail.com>
2016-02-19 18:52:25 +0330

    Create pluginlist.lua

---
+++ plugins/pluginlist.lua
@@ -0,0 +1,60 @@
+do
+
+function run(msg, matches)
+  return
+    inrealm
+    ingroup
+    inpm
+    banhammer
+    Feedback
+    lock_join
+    antilink
+    antitag
+    gps
+    leave
+    tagall
+    arabic_lock
+    welcome
+    google
+    sms
+    chat
+    Debian_service
+    sudoers
+    add_admin
+    anti_spam
+    add_bot
+    owners
+    set
+    get
+    broadcast
+    download_media
+    invite
+    all
+    leave_ban
+    danestani
+    joke
+    nerkh
+    echo
+    linkpv
+    antifosh
+    calculato
+    setrank
+    tex
+    map
+    share-contact
+    qr
+    on-off
+end
+
+return {
+  description = "plugin", 
+  usage = "/pluginlist",
+  patterns = {
+    "^[Pp]luginlist",
+    "^[!/]pluginlist$",
+
+  },
+  run = run
+}
+
+end
